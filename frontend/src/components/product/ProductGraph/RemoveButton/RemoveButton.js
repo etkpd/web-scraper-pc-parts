610de5cca2f3a311d9ddee5aeb5753a423378d31
etkpd <40926506+etkpd@users.noreply.github.com>
2019-07-10 16:28:14 -0400

    Frontend: styled button for removing parts from favorites window

--- frontend/src/components/product/ProductGraph/RemoveButton/RemoveButton.js
+++ frontend/src/components/product/ProductGraph/RemoveButton/RemoveButton.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import btnremoveStyles from './RemoveButton.module.scss'
 
-const RemoveButton = () => {
-  return (
-    <div>
-     <button>X</button>
-    </div>
-  );
-};
+const RemoveButton = () => (
+<div className={btnremoveStyles.button}>
+  <span className={btnremoveStyles.left}></span>
+  <span className={btnremoveStyles.right}></span>
+</div>
+);
+
 
 export default RemoveButton;
