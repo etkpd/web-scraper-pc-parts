eb6e4f0a4f4a68a1d53ea8c2000850b85300c75d
etkpd <40926506+etkpd@users.noreply.github.com>
2019-07-15 16:14:47 -0400

    Frontend: created UI for form used to submit part hyperlinks

--- frontend/src/components/cards/SubmitLinkCard/SubmitLinkCard.js
+++ frontend/src/components/cards/SubmitLinkCard/SubmitLinkCard.js
@@ -6,12 +6,16 @@ import * as Button from '../../buttons/Button';
 const SubmitLinkCard = () => {
   return (
     <div className={submit_linkStyles.card}>
-      <RemoveBtn/>
-      <p>Copy and paste product page you want to follow.</p>
-      <input type="text" className={submit_linkStyles.submitInput} placeholder="https://pcpartpicker.com/product/FQ648d" value=""></input>
-      <Button.Secondary
-        label='Submit'
-      />
+      <div className={submit_linkStyles.close}>
+        <RemoveBtn/>
+      </div>
+      <div className={submit_linkStyles.content}>
+        <p className={submit_linkStyles.prompt}>Copy and paste product page you want to follow.</p>
+        <input type="text" className={submit_linkStyles.submitInput} placeholder="https://pcpartpicker.com/product/FQ648d" value=""></input>
+        <Button.Secondary
+          label='Submit'
+          />
+      </div>
     </div>
   );
 };
